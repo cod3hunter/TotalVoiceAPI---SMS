23010353975b8241ecb8114f688e60d6ad3651a9
danielbx1 <danielmattos1020@gmail.com>
2017-06-28 19:51:52 -0300

    commit

---
+++ enviar_sms.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once "TotalVoiceAPI.class.php";
+
+$telefone_dest = $_POST['telefone']; //Recupera telefone por POST
+$msg = $_POST['msg']; //Recupera mensagem por POST
+
+//Instancia TotalVoiceAPI recebendo como parametro o Token disponivel em https://api.totalvoice.com.br/painel
+$api = new TotalVoiceAPI("8fb839a6d2ab9c3e4901a21dd581b265");
+
+ if (strlen($telefone_dest) >= 10 && // Verifica se os campos
+     strlen($telefone_dest) <= 11){ // foram preenchidos corretamente
+
+       if (strlen($msg) == 0) { //verifica se a mensagem foi digitada
+         echo "mensagem invalida";
+         header('Location: index.php?error=725');
+       }
+
+  $api->debugOn(); //Modo debug ativado
+
+  $api->returnAssoc(); //configura o retorno dos metodos como um array associativo, exemplo: $retorno['msg']
+
+  $result = $api->enviaSMS($telefone_dest, $msg); //Passa o resultado da requisição para a variavel $result
+
+  if ($result['status'] == 200) { //Verifica se a mensagem foi enviada com sucesso
+      header('Location: index.php?success=1');
+  }else { //executa o código abaixo caso não tenha sucesso no envio da mensagem
+      header('Location: index.php?success=0');
+  }
+
+} else { // executa o código abaixo caso o telefone não estaja correto
+
+  header('Location: index.php?error=548');
+
+}
---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<?php
+
+  $erro = $_GET['error'];
+
+  if ($erro == 404) {
+    # code...
+  }
+
+?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="panel panel-primary">
+
+        <div class="panel-heading">
+          <center><h1>Enviar SMS</h1></center>
+        </div>
+        <div class="panel-body">
+
+          <form action="enviar_sms.php" method="post">
+
+              <div class="input-group col-md-12">
+                <label for="telefone">Telefone destinatário (obrigatório)</label>
+                <input type="text" class="form-control" name="telefone" id="telefone" maxlength="11" placeholder="EX: 48995642318">
+              </div>
+
+              <?php
+
+                $erro = $_GET['error'];
+
+                if ($erro == 548) {
+                  echo '<span style="color: red;">Telefone incorreto</span>';
+                  echo "<br>";
+                }
+
+              ?>
+
+              <br>
+              <div class="input-group col-md-12">
+                <label for="msg">Mensagem (obrigatório)</label>
+                <input type="text" class="form-control" name="msg" id="msg" maxlength="150" placeholder="Max 150 Caracteres">
+              </div>
+
+              <?php
+
+                $erro = $_GET['error'];
+
+                if ($erro == 725) {
+                  echo '<span style="color: red;">Digite uma mensagem</span>';
+                  echo "<br>";
+                }
+
+              ?>
+
+              <br>
+              <button type="submit" class="btn btn-primary col-md-12" id="btnEnviar">Enviar SMS</button>
+              <br>
+          </form>
+
+        </div>
+
+        <?php
+
+          $sucesso = $_GET['success'];
+
+          if ($sucesso == 1) {
+            echo '<center><span style="color: green; font-size:20px">
+            Mensagem enviada com sucesso
+            </span></center>';
+            echo "<br>";
+          } elseif ($sucesso == 0) {
+            echo '<center><span style="color: red; font-size:20px;">
+            Falha ao enviar mensagem tente novamente mais tarde
+            </span></center>';
+            echo "<br>";
+          }
+
+        ?>
+
+
+      </div>
+
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body></html>
+</html>
